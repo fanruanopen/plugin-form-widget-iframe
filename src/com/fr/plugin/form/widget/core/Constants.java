5fdf083d8a9ff4e443e12f50536e413bb89231e7
richie <richie@finereport.com>
2016-03-28 16:44:16 +0800

    网页框控件代码迁移到solution下

---
+++ src/com/fr/plugin/form/widget/core/Constants.java
@@ -0,0 +1,9 @@
+package com.fr.plugin.form.widget.core;
+
+/**
+ * Created by richie on 16/3/28.
+ */
+public class Constants {
+
+    public static final String PLUGIN_ID = "com.fr.solution.plugin.form.widget.rh.iframe";
+}
